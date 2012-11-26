bb3fdff157238eb4aa85390bcee48e5a95e3f733
Fernando Ortiz <fortiz2k@gmail.com>
2012-11-26 14:26:55 -0600

    change to float64 and implement CheckConsistency

--- opinion-helper.go
+++ opinion-helper.go
@@ -1,11 +1,26 @@
 package sublogic
 
 import (
+  "math"
 )
 
-func outOfRange(val float32) bool {
+func outOfRange(val float64) bool {
 	if (val < 0) || (val > 1) {
 		return true
 	}
 	return false
+}
+
+func adjust(x float64) float64 {
+    if x >= math.MaxFloat64 {
+        return math.MaxFloat64
+    }
+    if x == 0.0 {
+        return 0.0
+    }
+    return math.Floor(x * 100000000000.0 + 0.5) / 100000000000.0
+}
+
+func constrain(x float64) float64 {
+    return math.Min(1.0, math.Max(0.0, x))
 }
--- opinion.go
+++ opinion.go
@@ -1,14 +1,15 @@
 package sublogic
 
 import (
-	"errors"
+    "math"
+    "errors"
 )
 
 type Opinion struct {
-	Belief      float32
-	Disbelief   float32
-	Uncertainty float32
-	Baserate    float32
+	Belief      float64
+	Disbelief   float64
+	Uncertainty float64
+	Baserate    float64
 }
 
 // Creates a new opinion.
@@ -16,14 +17,14 @@ type Opinion struct {
 // Opinion must satisfy the following:
 // - belief + disbelief + uncertainty = 1
 // - Each component must be in the range of 0 to 1
-func NewOpinion(belief, disbelief, uncertainty, baserate float32) (*Opinion, error) {
+func NewOpinion(belief, disbelief, uncertainty, baserate float64) (*Opinion, error) {
 	// Test for valid opinion
 	if val := belief + disbelief + uncertainty; val != 1 {
-		return nil, errors.New("The sum of all opinion components doesn't equal 1")
+		return newEmptyOpinion(), errors.New("The sum of all opinion components doesn't equal 1")
 	}
 	
 	if outOfRange(belief) || outOfRange(disbelief) || outOfRange(uncertainty) || outOfRange(baserate) {
-		return nil, errors.New("Opinion component isn't in range of 0 to 1")
+		return newEmptyOpinion(), errors.New("Opinion component isn't in range of 0 to 1")
 	}
 	
 	opinion := Opinion{ belief, disbelief, uncertainty, baserate }
@@ -38,11 +39,25 @@ func newEmptyOpinion() (*Opinion) {
 func (A *Opinion) Discount(B *Opinion) (C *Opinion) {
 	C = newEmptyOpinion()
 	
-	// TODO check A and B for consistency
 	C.Belief = A.Belief * B.Belief
 	C.Disbelief = A.Belief * B.Disbelief
 	C.Uncertainty = (A.Disbelief + A.Uncertainty + A.Belief*B.Uncertainty)
 	C.Baserate = B.Baserate
+    
+    C.CheckConsistency()
 	
 	return C
+}
+
+func (this *Opinion) CheckConsistency() {
+    this.Belief = constrain(adjust(this.Belief))
+    this.Disbelief = constrain(adjust(this.Disbelief))
+    this.Uncertainty = constrain(adjust(this.Uncertainty))
+    if math.Abs(float64(this.Belief + this.Disbelief + this.Uncertainty - 1.0)) > 1.0E-010 {
+        var bdu float64
+        bdu = this.Belief + this.Disbelief + this.Uncertainty
+        this.Belief = constrain(adjust(this.Belief / bdu))
+        this.Uncertainty = constrain(adjust(this.Uncertainty / bdu))
+        this.Disbelief = (1.0 - (this.Belief + this.Uncertainty))
+    }
 }
